eb0d6b0e802b0dbe94d3b04c5ef59a10930e3c44
Felipe Ramirez <feliperamigon1704@gmail.com>
2017-01-30 12:45:00 -0500

    Version Inicial - Información Terminada

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#").fullpage();
+
+
+	$('#home').click(function(){
+		$('#information').hide();
+		$('#education').hide();
+		$('#portfolio').hide();
+		$('#contact').hide();
+	});
+});
